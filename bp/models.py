df4e966ecd4c9b57e44e1160922f1e23270e5a77
nargesghanei <nargesghanei031@gmail.com>
2021-03-04 19:41:14 +0330

    'firstchange'

--- bp/models.py
+++ bp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
+class student(models.Model):
+    studentcode=  models.IntegerField()
+    dateofsend=   models.DateField(null=True,blank=True)
+    score=        models.IntegerField()
 
-# Create your models here.
+    def get_absolute_url(self):
+        return reverse('studentcodedetail',args=[str(self.id)])
+    def __int__(self):
+        return self.studentcode
